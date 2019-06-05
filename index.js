372a15df06f92e9c222531a289612b7befa72089
Dominic Tremblay <dominictremblay24@gmail.com>
2019-06-05 12:02:54 -0400

    final demo may27

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const uuidv4 = require('uuid/v4');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const movieQuotesDb = {
   'd9424e04-9df6-4b76-86cc-9069ca8ee4bb': {
@@ -39,6 +42,72 @@ const quoteComments = {
 
 app.get('/', (req, res) => {
   res.send('Quotes App Homepage');
+});
+
+app.get('/quotes', (req, res) => {
+  res.render('quotes', { quotes: Object.values(movieQuotesDb) });
+});
+
+app.get('/quotes/new', (req, res) => {
+  res.render('quote_new');
+});
+
+app.post('/quotes', (req, res) => {
+  console.log('req.body', req.body);
+
+  // extracting the quote content from the form request
+  // es6 destructing
+  const { quote } = req.body;
+  // conventional
+  // const quote = req.body.quote;
+
+  const id = uuidv4();
+
+  // create a new quote object
+
+  const newQuote = {
+    id: id,
+    quote: quote,
+  };
+
+  // Add a new quote to the movieQuoteDb
+  movieQuotesDb[id] = newQuote;
+
+  // redirecting to the quotes page
+  res.redirect('/quotes');
+});
+
+app.get('/quotes/:id', (req, res) => {
+  const { id } = req.params;
+
+  const templateVars = { quote: movieQuotesDb[id] };
+
+  res.render('quote_show', templateVars);
+});
+
+app.post('/quotes/:id/delete', (req, res) => {
+  // extract the id info from the path
+  const { id } = req.params;
+  // const id = req.params.id;
+
+  delete movieQuotesDb[id];
+
+  res.redirect('/quotes');
+});
+
+app.post('/quotes/:id', (req, res) => {
+  // extract the id from the url
+  const { id } = req.params;
+
+  // extract the quote content from the form
+  const { quote } = req.body;
+
+  // update the quote in movieQuoteDb
+  movieQuotesDb[id].quote = quote;
+
+  // redirect to quotes
+
+  res.redirect('/quotes');
 });
 
 app.listen(port, () => console.log(`Express server listening on port ${port}`));
